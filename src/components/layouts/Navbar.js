27c404a125b0fc05f2e9135e7552bf3579f86c58
Rohail Ramesh <rohailramesh@hotmail.com>
2024-08-16 14:22:24 +0100

    Feat: Added dark and light theme

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -5,7 +5,10 @@ import Link from "next/link";
 import React, { useContext, useEffect, useState } from "react";
 
 function Navbar() {
+  //   const [mounted, setMounted] = useState(false);
   //   const { state } = useContext(CartContext);
+  const { theme, setTheme } = useTheme();
+
   return (
     <header className="text-white-100 sticky top-0 z-50 bg-gradient-to-r from-indigo-700 via-violet-700 to-orange-700 body-font">
       <div className="container mx-auto flex flex-wrap  p-3 flex-col md:flex-row items-center">
@@ -101,6 +104,40 @@ function Navbar() {
             </svg>
           </Link>
         </nav>
+        <button
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          className="text-white bg-black rounded-full p-1  dark:text-black dark:bg-white flex items-center"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-4 h-4"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+            />
+          </svg>{" "}
+          /
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-5 h-5"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+            />
+          </svg>
+        </button>
       </div>
     </header>
   );
